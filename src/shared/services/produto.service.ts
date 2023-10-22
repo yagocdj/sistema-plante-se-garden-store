d4c9f6ef9c5646cb78e6a2568addc49cab1d8771
Renato Melo <renato.bmelo@outlook.com>
2023-10-22 17:28:33 -0300

    Iniciando CRUD de produto

--- src/shared/services/produto.service.ts
+++ src/shared/services/produto.service.ts
@@ -1,3 +1,5 @@
+import { PRODUTOS } from './../model/produtos';
+import { Produto } from './../model/produto';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,26 @@ import { Injectable } from '@angular/core';
 })
 export class ProdutoService {
 
-  constructor() { }
+  private _produtos: Array<Produto>;
+
+  // URL_PRODUTOS = 'http://localhost:3000/produtos';
+  //constructor(private httpClient:HttpClient){}
+  constructor() {
+    this._produtos = PRODUTOS;
+   }
+
+  listar():Array<Produto> {
+    return this._produtos;
+  }
+
+  inserir(produto: Produto):void{
+    //aqui precisa ter o tratamento de exceção
+    this._produtos.push(produto);
+  }
+
+  localizar(){}
+
+  remover(){}
+
+  editar(){}
 }
