59465db884ea0b38285a91975a94d2006ca4c14a
Yago César <yagocdj@gmail.com>
2023-11-02 19:26:30 -0300

    add a search box to the mobile menu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,7 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexModule } from '@angular/flex-layout';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { FlexModule } from '@angular/flex-layout';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    FlexModule
+    FlexModule,
+    MatSidenavModule,
+    MatInputModule
   ],
   exports: [
     MenuComponent
