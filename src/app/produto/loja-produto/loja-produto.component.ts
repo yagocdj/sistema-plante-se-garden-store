fb7542b8287c28cab6accda1b6c0bfa15c6ce400
Yago César <yagocdj@gmail.com>
2023-11-07 23:07:29 -0300

    add a mat-grid-list to 'loja-produto' component; adjust router-outlet tag location; put the task#13's 'produto' service inside a service in this task

--- src/app/produto/loja-produto/loja-produto.component.ts
+++ src/app/produto/loja-produto/loja-produto.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/shared/model/produto';
+import { ProdutoService } from 'src/app/shared/services/produto.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-loja-produto',
   templateUrl: './loja-produto.component.html',
   styleUrls: ['./loja-produto.component.scss']
 })
-export class LojaProdutoComponent {
+export class LojaProdutoComponent implements OnInit {
+  productsInfo: Produto[];
+  numberOfCols: number;
 
+  constructor(
+    private produtoService: ProdutoService,
+    private responsive: BreakpointObserver
+  ) {
+    this.productsInfo = []
+    this.numberOfCols = 2;
+  }
+
+  ngOnInit(): void {
+    this.fetchAllProducts();
+    this.responsive.observe([
+      '(max-width: 695px)', '(max-width: 954px)'
+    ]).subscribe(
+      result => {
+        const breakpoints = result.breakpoints;
+        
+        if (breakpoints['(max-width: 695px)']) {
+          this.numberOfCols = 2;
+        } else if (breakpoints['(max-width: 954px)']) {
+          this.numberOfCols = 3;
+        } else {
+          this.numberOfCols = 4;
+        }
+      }
+    );
+    
+  }
+
+  fetchAllProducts(): void {
+    this.produtoService.listar().subscribe(
+      products => products.forEach(
+        p => this.productsInfo.push(p)
+      )
+    );
+  }
+  
 }
--- src/app/produto/produto.module.ts
+++ src/app/produto/produto.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LojaProdutoComponent } from './loja-produto/loja-produto.component';
 import { MatCardModule } from '@angular/material/card';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,9 @@ import { MatCardModule } from '@angular/material/card';
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule
   ]
 })
 export class ProdutoModule { }
