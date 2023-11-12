87c518ce09545763d34d600022070e2bc09d941f
Renato Melo <renato.bmelo@outlook.com>
2023-11-12 17:27:01 -0300

    Listagem de Cliente para review

---
+++ src/app/cliente/listagem-cliente/listagem-cliente.component.ts
@@ -0,0 +1,46 @@
+import { ClienteService } from 'src/app/shared/services/cliente.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Cliente } from 'src/app/shared/model/cliente';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+@Component({
+  selector: 'app-listagem-cliente',
+  templateUrl: './listagem-cliente.component.html',
+  styleUrls: ['./listagem-cliente.component.scss']
+})
+
+export class ListagemClienteComponent implements OnInit{
+  titulo = "Área de Clientes";
+  clientes: Array<Cliente> = [];
+  dataSource = new MatTableDataSource<any>;
+  displayedColumns: string[] = ['ID', 'Nome', 'CPF', 'Email', 'Telefone']
+
+
+  @ViewChild(MatPaginator) paginator!:MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private clienteService:ClienteService){
+  }
+
+  ngOnInit(): void{
+    this.clienteService.listar().subscribe(
+      {next:(res)=>{this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+      })
+  }
+
+  //default
+  aplicarFiltro(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+}
+
+
+
