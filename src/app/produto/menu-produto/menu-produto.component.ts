63886f0e544435c61afe1dcb3a8bb89cb08a6bac
Renato Melo <renato.bmelo@outlook.com>
2023-12-21 00:11:51 -0300

    pesquisarporNome - produto

--- src/app/produto/menu-produto/menu-produto.component.ts
+++ src/app/produto/menu-produto/menu-produto.component.ts
@@ -1,6 +1,8 @@
+import { ProdutoService } from 'src/app/shared/services/produto.service';
 import { Component,Inject  } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CadastrarProdutoComponent } from '../cadastrar-produto/cadastrar-produto.component';
+import { Produto } from 'src/app/shared/model/produto';
 
 @Component({
   selector: 'app-menu-produto',
@@ -8,19 +10,29 @@ import { CadastrarProdutoComponent } from '../cadastrar-produto/cadastrar-produt
   styleUrls: ['./menu-produto.component.scss']
 })
 export class MenuProdutoComponent {
+  produtos: Produto[] = [];
+  produtosPesquisa: Array<Produto> = [];
+  nomePesquisado: string = '';
+  produtosPesquisados: Produto[] = [];
 
-constructor(private dialog: MatDialog) {
+constructor(private dialog: MatDialog, private produtoService: ProdutoService) {
 }
 
 openDialog() {
   this.dialog.open(CadastrarProdutoComponent, {
     width:'16%'
   });
-  
+
 }
 
 refreshPage(){
   location.reload();
 }
 
+pesquisar() {
+  this.produtoService.pesquisarPorNome(this.nomePesquisado).subscribe((produtos: Produto[]) => {
+    this.produtosPesquisados = produtos;
+    console.log(produtos);
+  });
+}
 }
